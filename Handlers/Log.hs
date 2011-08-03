1ddf798282ed94e8c0ade322a8504c62255e4077
Pascal Wittmann <mail@pascal-wittmann.de>
2011-08-03 12:48:44 +0200

    moved NewLog.hs to Log.hs and fixed nasty width bug in css

--- Handlers/Log.hs
+++ Handlers/Log.hs
@@ -3,15 +3,26 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Handlers.Log
-       (
-         getLogR
-         ) where
+       ( getLogR
+       , getNewLogR
+       , postNewLogR
+       ) where
 
 import Homepage
 import qualified Settings
 import Model
 import Yesod
+import Yesod.Auth
 import Yesod.Goodies.Markdown
+
+import Control.Applicative
+import Data.Text (Text)
+import Data.Time
+
+data Params = Params
+     { title :: Text
+     , text :: Markdown
+     }
 
 getLogR :: Handler RepHtml
 getLogR = do 
@@ -19,3 +30,30 @@ getLogR = do
   defaultLayout $ do
   setTitle "Log"
   $(Settings.hamletFile "log")
+     
+paramsFormlet :: Maybe Params -> Form s m Params
+paramsFormlet mparams = fieldsToTable $ Params
+    <$> stringField "Title" (fmap title mparams)
+    <*> markdownField "Text" (fmap text mparams)
+
+getNewLogR :: Handler RepHtml
+getNewLogR = do
+  requireAuth
+  (_, form, enctype) <- runFormGet $ paramsFormlet Nothing
+  defaultLayout $ do
+    $(Settings.hamletFile "newlog")
+
+
+postNewLogR :: Handler ()
+postNewLogR = do
+  (uid, _) <- requireAuth
+  (res, _, _) <- runFormPostNoNonce $ paramsFormlet Nothing
+  case res of
+    FormSuccess (Params title text) -> do
+      now <- liftIO getCurrentTime
+      runDB $ insert $ Article title text "" now
+      redirect RedirectTemporary LogR
+      return ()
+    _ -> do
+      redirect RedirectTemporary NewLogR
+      return ()
