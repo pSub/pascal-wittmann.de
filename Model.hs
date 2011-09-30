18cb4c55935212d0ad3c153ec43ed1e3fdaf736d
Pascal Wittmann <mail@pascal-wittmann.de>
2011-09-30 13:18:02 +0200

    removed empty line

--- Model.hs
+++ Model.hs
@@ -6,7 +6,6 @@ import Data.Text (Text)
 import Data.Time
 import Yesod.Goodies.Markdown
 
-
 -- You can define all of your database entities in the entities file.
 -- You can find more information on persistent and how to declare entities
 -- at:
