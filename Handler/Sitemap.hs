53ced2e5ca1110af77fb1170c467d922140d9e75
Pascal Wittmann <mail@pascal-wittmann.de>
2012-04-08 20:33:02 +0200

    calculate lastMod for EntriesR

--- Handler/Sitemap.hs
+++ Handler/Sitemap.hs
@@ -12,18 +12,19 @@ getSitemapR :: Handler RepXml
 getSitemapR = do
     time <- liftIO getCurrentTime
     categories <- runDB $ selectList [] []
+    entries <- runDB $ selectList [] []
     categories_urls <- return $ map (\(Entity _ v) -> SitemapUrl
                     { sitemapLoc = EntriesR $ categoryName v
-                    , sitemapLastMod = time
+                    , sitemapLastMod = maximum $ map (entryLastMod . entityVal) entries
                     , sitemapChangeFreq = Weekly
                     , sitemapPriority = 0.7
                     }) categories
-    entries <- runDB $ selectList [] [] >>= return . map (\(Entity _ v) -> SitemapUrl
+    entries_urls <- return $ map (\(Entity _ v) -> SitemapUrl
             { sitemapLoc = EntryR (findCategory (entryCat v) categories) $ entryIdent v
             , sitemapLastMod = entryLastMod v
             , sitemapChangeFreq = Monthly
             , sitemapPriority = 0.9
-            })
+            }) entries
     statics <- runDB $ selectList [] [] >>= return . map(\(Entity _ v) -> SitemapUrl
             { sitemapLoc = StaticR $ StaticRoute [attachmentFile v] []
             , sitemapLastMod = time
@@ -32,5 +33,5 @@ getSitemapR = do
             })
     sitemap $ [ SitemapUrl RootR time Monthly 1.0
               , SitemapUrl ImpressumR time Yearly 0.2
-              ] ++ categories_urls ++ entries ++ statics
+              ] ++ categories_urls ++ entries_urls ++ statics
    where findCategory eid = categoryName . entityVal . fromJust . find ((eid ==) . entityKey)
