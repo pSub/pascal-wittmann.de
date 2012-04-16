8c97433a0968285faf9dc6a435060d7e142db80b
Pascal Wittmann <mail@pascal-wittmann.de>
2012-04-16 22:36:05 +0200

    exclude some Prelude functions and imported Settings.StaticFiles

--- Import.hs
+++ Import.hs
@@ -8,12 +8,13 @@ module Import
     , module Control.Applicative
     ) where
 
-import Prelude hiding (writeFile, readFile)
+import Prelude hiding (writeFile, readFile, head, tail, init, last)
 import Yesod   hiding (Route(..))
 import Foundation
 import Data.Monoid (Monoid (mappend, mempty, mconcat))
 import Control.Applicative ((<$>), (<*>), pure)
 import Data.Text (Text)
+import Settings.StaticFiles
 
 infixr 5 <>
 (<>) :: Monoid m => m -> m -> m
