c943fc13643717d78b829c0a65037dfd8784d56f
Pascal Wittmann <mail@pascal-wittmann.de>
2012-04-19 23:18:09 +0200

    delete form for categories

--- Handler/Admin.hs
+++ Handler/Admin.hs
@@ -6,21 +6,35 @@ module Handler.Admin
 
 import Import
 import Database.Persist.Store
+import Control.Arrow
 
 formletCat :: Maybe Category -> Form Category
 formletCat mparams = renderDivs $ Category
     <$> areq textField "Kategorie" (categoryName <$> mparams)
 
+deleteCatForm :: [(Text, CategoryId)] -> Form [CategoryId]
+deleteCatForm cats = renderDivs $ areq (multiSelectFieldList cats) "" Nothing
+
 getAdminR :: Handler RepHtml
 getAdminR = do
   requireAdmin
-  cats <- runDB $ selectList [] [Asc CategoryName]
   ((res, catForm), catEnctype) <- runFormPost $ formletCat Nothing
   case res of
     FormSuccess cat -> do
       _ <- runDB $ insert cat
       redirect AdminR
     _ -> return ()
+
+  cats <- runDB $ selectList [] [Asc CategoryName]
+  let opts = map ((categoryName . entityVal) &&& entityKey) cats
+  -- FIXME: used formGet because the identifiers of both forms clash
+  ((deleteRes, deleteCatForm), deleteEnctype) <- runFormGet $ deleteCatForm opts
+  case deleteRes of
+    FormSuccess ckeys -> do
+      runDB $ deleteCascadeWhere [FilterOr $ map (CategoryId ==.) ckeys]
+      redirect AdminR
+    _ -> return ()
+
   defaultLayout $ do
     setTitle "Admin"
     $(widgetFile "admin")
