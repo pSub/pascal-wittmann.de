5ef3f5c142bf3ac4473836092f1b9174cc663602
Pascal Wittmann <mail@pascal-wittmann.de>
2013-01-23 18:57:44 +0100

    prepare for tests

---
+++ tests/HomeTest.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE OverloadedStrings #-}
+module HomeTest
+    ( homeSpecs
+    ) where
+
+import           TestImport
+
+homeSpecs :: Specs
+homeSpecs =
+  describe "These are some example tests" $
+    it "loads the index and checks it looks right" $ do
+      get_ "/"
+      statusIs 200
+
+      --post "/" $ do
+      --  addNonce
+      --  fileByLabel "Choose a file" "tests/main.hs" "text/plain" -- talk about self-reference
+      --  byLabel "What's on the file?" "Some Content"
+
+      --statusIs 200
+      --htmlCount ".message" 1
+      --htmlAllContain ".message" "Some Content"
+      --htmlAllContain ".message" "text/plain"
