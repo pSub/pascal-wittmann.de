589cf39683e10f3951ea338b4a48d4583b9103f3
Pascal Wittmann <mail@pascal-wittmann.de>
2015-10-07 11:41:21 +0200

    Add Import/NoFoundation.hs

---
+++ Import/NoFoundation.hs
@@ -0,0 +1,15 @@
+module Import.NoFoundation
+    ( module Import
+    ) where
+
+-- FIXME: Is there some non-hacky way to fix conflicts between ClassyPrelude.Yesod and Esqueleto?
+import ClassyPrelude.Yesod   as Import hiding (Value, delete, isNothing, on, (!=.), (*=.), (+=.), (-=.), (/=.), (<.), (<=.), (>.), (>=.), (==.), (=.), (||.), selectSource, groupBy, count, update)
+import Model                 as Import
+import Settings              as Import
+import Settings.StaticFiles  as Import
+import Yesod.Auth            as Import
+import Yesod.Core.Types      as Import (loggerSet)
+import Yesod.Default.Config2 as Import
+
+-- Custom imports
+import Database.Esqueleto as Import -- conflicts with ClassPrelude.Yesod
