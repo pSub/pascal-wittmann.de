8f21614019fc0945777d04b5235dc64d2f79fb0e
Pascal Wittmann <mail@pascal-wittmann.de>
2016-03-25 17:47:11 +0100

    An attempt to fix travis-ci

---
+++ exec.sh
@@ -0,0 +1,26 @@
+#! /usr/bin/env bash
+set -e
+
+export NIX_CURL_FLAGS=-sS
+
+if [[ $1 == nix ]]; then
+    echo "=== Installing Nix..."
+    # Install Nix
+    bash <(curl -sS https://nixos.org/nix/install)
+    source $HOME/.nix-profile/etc/profile.d/nix.sh
+
+    # Make sure we can use hydra's binary cache
+    sudo mkdir /etc/nix
+    sudo tee /etc/nix/nix.conf <<EOF >/dev/null
+binary-caches = http://cache.nixos.org http://hydra.nixos.org
+trusted-binary-caches = http://hydra.nixos.org
+build-max-jobs = 4
+EOF
+
+elif [[ $1 == build ]]; then
+    source $HOME/.nix-profile/etc/profile.d/nix.sh
+    nix-build --argstr compiler "ghc7103"
+else
+    echo "$0: Unknown option $1" >&2
+    false
+fi
