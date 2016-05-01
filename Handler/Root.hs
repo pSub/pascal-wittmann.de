73fbdcfe8c3862aa45fa9e10a698500234e0e90b
Pascal Wittmann <mail@pascal-wittmann.de>
2016-05-01 14:18:46 +0200

    Show the #ilovefs banner at valentines day

--- Handler/Root.hs
+++ Handler/Root.hs
@@ -2,6 +2,7 @@ module Handler.Root where
 
 import           Data.List as L (find)
 import           Import
+import           Data.Time.LocalTime
 
 getRootR :: Handler Html
 getRootR = do
@@ -12,10 +13,16 @@ getRootR = do
   cats <- runDB $ select $ from $ \c -> do
                   orderBy [asc (c ^. CategoryName)]
                   return c
+  zonedTime <- liftIO getZonedTime
   defaultLayout $ do
     setTitle "Pascal Wittmann"
     $(widgetFile "root")
   where findCat p cs = L.find (\ c -> entityKey c == entryCat p) cs
+
+isValentinesDay :: ZonedTime -> Bool
+isValentinesDay (ZonedTime (LocalTime localDay _) _) =
+  let (_, month, day) = toGregorian localDay
+      in month == 2 && day == 14
 
 nofacebookme_png :: StaticRoute
 nofacebookme_png = StaticRoute ["no-facebook-me.png"] []
