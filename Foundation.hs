58333910a728ca7298b65a9528db6e5ad9e29b52
Pascal Wittmann <mail@pascal-wittmann.de>
2016-10-09 09:20:53 +0200

    Make Cookies Secure

--- Foundation.hs
+++ Foundation.hs
@@ -62,7 +62,7 @@ instance Yesod App where
 
     -- Store session data on the client in encrypted cookies,
     -- default session idle timeout is 120 minutes
-    makeSessionBackend _ = Just <$> defaultClientSessionBackend
+    makeSessionBackend _ = sslOnlySessions $ Just <$> defaultClientSessionBackend
          120 -- timeout in minutes
          "config/client_session_key.aes"
 
