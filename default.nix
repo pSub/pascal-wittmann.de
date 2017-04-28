750251b845a2ea9cd4a1ebb96ce7a792855e19e9
Pascal Wittmann <pascal.wittmann@red6-es.de>
2017-04-28 15:24:26 +0200

    Use ghc802 and nixos-17.03

--- default.nix
+++ default.nix
@@ -1,2 +1,2 @@
-{ compiler ? "ghc7103", nixpkgs ? import <nixpkgs> {} }:
+{ compiler ? "ghc802", nixpkgs ? import <nixpkgs> {} }:
 nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./homepage.nix { }
