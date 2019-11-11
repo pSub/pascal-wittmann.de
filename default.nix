80d0335e21500cb5a4206df7704efca81fee6c2a
Pascal Wittmann <mail@pascal-wittmann.de>
2019-11-11 20:27:18 +0100

    Update to nixos-19.09

--- default.nix
+++ default.nix
@@ -1,2 +1,2 @@
-{ compiler ? "ghc864", nixpkgs ? import <nixpkgs> {} }:
+{ compiler ? "ghc865", nixpkgs ? import <nixpkgs> {} }:
 nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./homepage.nix { }
