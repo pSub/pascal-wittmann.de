7a568f8701f4f03249db07f2766e10aaf0a9c145
Pascal Wittmann <pascal.wittmann@red6-es.de>
2022-02-04 14:57:59 +0100

    Build with NixOS 21.11

--- default.nix
+++ default.nix
@@ -1,2 +1,2 @@
-{ compiler ? "ghc8104", nixpkgs ? import <nixpkgs> {} }:
+{ compiler ? "ghc8107", nixpkgs ? import <nixpkgs> {} }:
 nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./homepage.nix { }
