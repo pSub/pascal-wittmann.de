a0e3e9645b711bf017d2e52abf8260b565d7364d
Pascal Wittmann <mail@pascal-wittmann.de>
2024-02-09 23:09:22 +0100

    Move to ghc-9.4 and use custom yesod-csp

--- default.nix
+++ default.nix
@@ -1,2 +1,18 @@
-{ compiler ? "ghc8107", nixpkgs ? import <nixpkgs> {} }:
-nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./homepage.nix { }
+{ compiler ? "ghc94", nixpkgs ? import <nixpkgs> {} }:
+let
+  # Use my fork as long as github.com/bobjflong/yesod-csp/issues/8 is
+  # not fixed.
+  yesod-csp = (
+    import (
+      nixpkgs.fetchFromGitHub {
+        owner = "pSub";
+        repo = "yesod-csp";
+        rev = "011d52d1f4a8415ae486f3c38f3935c778a3878d";
+        hash = "sha256-4I9toFJBvw1rzVCAG/qJBJwT2I946SvL9r8aNLN5eAA=";
+      }
+    )
+  ) { inherit nixpkgs; };
+
+in
+
+nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./homepage.nix { inherit yesod-csp; }
