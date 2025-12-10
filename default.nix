8546abd510b0427808aba8ac235339db130e17c9
Pascal Wittmann <pascal.wittmann@red6-es.de>
2025-12-10 10:26:36 +0100

    Migrate to NixOS 25.11

--- default.nix
+++ default.nix
@@ -1,3 +1,20 @@
-{ compiler ? "ghc98", nixpkgs ? import <nixpkgs> {} }:
+{
+  compiler ? "ghc910",
+  nixpkgs ? import <nixpkgs> { },
+}:
 
-nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./homepage.nix {  }
+let
+  # Remove when the build is fixed on nixos-25.11
+  yesod-csp =
+    (import (
+      nixpkgs.fetchFromGitHub {
+        owner = "pSub";
+        repo = "yesod-csp";
+        rev = "f15a2dcdb9ae069360326895c00736333f564378";
+        hash = "sha256-w4k45nXHX5zhLKDOw8NQ2aSM2pRzXQFb+9A/B5b65xo=";
+      }
+    ))
+      { inherit nixpkgs; };
+
+in
+nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./homepage.nix { inherit yesod-csp; }
