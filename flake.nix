2b29230758430303d2fdc59090baab2180394055
Pascal Wittmann <pascal.wittmann@red6-es.de>
2025-12-27 22:45:13 +0100

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Homepage pascal-wittmann.de";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        packages.homepage = pkgs.callPackage ./default.nix { nixpkgs = pkgs; };
+      }
+    );
+}
