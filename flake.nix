45b3675c4eb6f6deb1078e0db49b2115242eca7b
Pascal Wittmann <pascal.wittmann@red6-es.de>
2025-12-28 18:56:50 +0100

    Update flake

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     flake-utils.url = "github:numtide/flake-utils";
+    yesod-csp-nix.url = "github:pSub/yesod-csp";
   };
 
   outputs =
@@ -11,6 +12,7 @@
       self,
       nixpkgs,
       flake-utils,
+      yesod-csp-nix
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
@@ -18,7 +20,9 @@
         pkgs = import nixpkgs { inherit system; };
       in
       {
-        packages.homepage = pkgs.callPackage ./default.nix { nixpkgs = pkgs; };
+        packages.homepage = nixpkgs.pkgs.haskell.packages.ghc910.callPackage ./homepage.nix {
+          yesod-csp = yesod-csp-nix.packages.${system}.yesod-csp;
+        };
       }
     );
 }
