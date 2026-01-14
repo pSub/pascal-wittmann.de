b562c2a739b3279587a06c3b8a36d4af5f5a9734
Pascal Wittmann <pascal.wittmann@red6-es.de>
2026-01-14 15:38:47 +0100

    Update flake.lock

--- flake.nix
+++ flake.nix
@@ -4,7 +4,9 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     flake-utils.url = "github:numtide/flake-utils";
+    flake-utils.inputs.nixpkgs.follows = "nixpkgs";
     yesod-csp-nix.url = "github:pSub/yesod-csp";
+    yesod-csp-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs =
